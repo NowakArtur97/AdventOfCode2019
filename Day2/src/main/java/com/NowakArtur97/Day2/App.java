6255927125d8cbc8200987530626b7f72100e096
NowakArtur97 <nowart97@gmail.com>
2019-12-01 10:58:51 +0100

    Init for Day 2

---
+++ Day2/src/main/java/com/NowakArtur97/Day2/App.java
@@ -0,0 +1,13 @@
+package com.NowakArtur97.Day2;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
