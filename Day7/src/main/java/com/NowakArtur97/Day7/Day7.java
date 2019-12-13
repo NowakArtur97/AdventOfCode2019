f741c3832834dc9de758658e12abe30ae8b0a6f9
NowakArtur97 <nowart97@gmail.com>
2019-12-13 14:12:08 +0100

    Init for Day 7

---
+++ Day7/src/main/java/com/NowakArtur97/Day7/Day7.java
@@ -0,0 +1,13 @@
+package com.NowakArtur97.Day7;
+
+/**
+ * Hello world!
+ *
+ */
+public class Day7 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
